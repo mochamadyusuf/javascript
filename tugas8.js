0b929b860869a416e5e78397a9f16df2096b379e
mochamadyusuf <mochamadd.yusuf@gmail.com>
2022-07-10 23:46:09 +0700

    menambahkan file tugas8.js

---
+++ tugas8.js
@@ -0,0 +1,11 @@
+const mahasiswa = {
+  nama: "Mochamad Yusuf",
+  nim: 1544390049,
+  jurusan: "Sistem Informasi",
+};
+
+let mhs = mahasiswa;
+console.log(mhs);
+
+mahasiswa.jurusan = "Teknik Informatika";
+console.log(mhs);
